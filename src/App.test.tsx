922d1e92457923e1267f902aeaad6a885c172d7d
Victor <victor_github@outlook.com>
2021-09-21 21:54:22 +0200

    Test fixed.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
 	render(<App />);
-	const textElement = screen.getByText(/Order Book:/i);
+	const textElement = screen.getByText(/Order Book/i);
 	expect(textElement).toBeInTheDocument();
 });
