02dd99fd7b6bcde3a9795e57d57f56b029d4cefb
Victor Rios <victor_github@outlook.com>
2021-09-22 01:12:59 +0200

    Cleanup.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const textElement = screen.getByText(/Order Book/i);
-  expect(textElement).toBeInTheDocument();
+	render(<App />);
+	const textElement = screen.getByText(/Order Book/i);
+	expect(textElement).toBeInTheDocument();
 });
--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,10 +1,5 @@
-export function userLocale(): string {
-  const html = document.getElementsByTagName("html")[0];
-  return (html && html.getAttribute("data-locale")) || "en-US";
-}
-
 export function toFormattedNumber(value: number, digits?: number) {
-  return new Intl.NumberFormat(userLocale(), {
-    minimumFractionDigits: digits,
-  }).format(value);
+	return new Intl.NumberFormat(undefined, {
+		minimumFractionDigits: digits,
+	}).format(value);
 }
